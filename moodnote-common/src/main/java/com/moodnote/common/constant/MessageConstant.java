ac9898439e225f9daeae8a6797866a96b60b3438
TE-Fire <3037749727@qq.com>
2026-05-07 21:59:04 +0800

    feat(常量): 添加邮件和验证码相关的消息常量
    添加用于邮件发送、验证码验证等场景的常量字符串，方便统一管理和维护错误消息和状态信息

---
+++ moodnote-common/src/main/java/com/moodnote/common/constant/MessageConstant.java
@@ -0,0 +1,19 @@
+package com.moodnote.common.constant;
+
+public class MessageConstant {
+
+    public static final String EMAIL_SEND_SUCCESS = "邮件发送成功";
+    public static final String EMAIL_SEND_FAILED = "邮件发送失败";
+    public static final String EMAIL_FORMAT_ERROR = "邮箱格式不正确";
+    public static final String EMAIL_EMPTY = "邮箱不能为空";
+    public static final String CODE_SEND_SUCCESS = "验证码发送成功，请查收邮件";
+    public static final String CODE_SEND_FAILED = "发送失败，请稍后重试";
+    public static final String CODE_EXPIRE = "验证码已过期，请重新获取";
+    public static final String CODE_ERROR = "验证码错误";
+
+    public static final String LOGIN = "login";
+    public static final String RESET = "reset";
+    public static final String REGISTER = "register";
+
+
+}
