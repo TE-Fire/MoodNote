8b23d76c08da2a32de61ed5f67809ecacafddc68
TE-Fire <3037749727@qq.com>
2026-05-09 17:04:13 +0800

    feat(用户模块): 添加用户相关数据库操作接口
    添加用户Mapper接口及XML文件，实现用户邮箱和用户名存在性检查以及用户插入功能

--- moodnote-mapper/src/main/java/com/moodnote/mapper/UserMapper.java
+++ moodnote-mapper/src/main/java/com/moodnote/mapper/UserMapper.java
@@ -1,17 +1,25 @@
 package com.moodnote.mapper;
 
 import com.moodnote.pojo.entity.User;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
 
 @Mapper
 public interface UserMapper {
 
-    User selectByUsername(@Param("username") String username);
+    // language=SQL
+    @Select("select exists(select 1 from mood_user where email = #{email} and deleted = 0)")
+    boolean existByEmail(@Param("email") String email);
 
-    User selectByEmail(@Param("email") String email);
+    // language=SQL
+    @Select("select exists(select 1 from mood_user where username = #{username} and deleted = 0)")
+    boolean existByUsername(@Param("username") String username);
 
-    User selectByUsernameOrEmail(@Param("username") String username);
-
-    void updateLastLoginTime(@Param("id") Long id);
+    // language=SQL
+    @Insert("insert into mood_user (username, password, email, nickname, avatar, gender, create_time, update_time) " +
+            "values (#{user.username}, #{user.password}, #{user.email}, #{user.nickname}, #{user.avatar}, #{user.gender}, #{user.createTime}, #{user.updateTime})")
+    int insert(@Param("user") User user);
 }
