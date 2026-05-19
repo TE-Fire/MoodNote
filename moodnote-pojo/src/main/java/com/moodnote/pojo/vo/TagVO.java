d4858e560530e8e4abf17ddba54037b35e05ee97
TE-Fire <3037749727@qq.com>
2026-05-20 00:05:18 +0800

    fix(TagVO): 给diaryId添加JsonIgnore注解避免返回前端
    给TagVO的diaryId字段加上Jackson的JsonIgnore注解，防止该字段被序列化后返回给前端，符合接口返回数据的设计预期。

--- moodnote-pojo/src/main/java/com/moodnote/pojo/vo/TagVO.java
+++ moodnote-pojo/src/main/java/com/moodnote/pojo/vo/TagVO.java
@@ -4,8 +4,11 @@ import lombok.Data;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Data
 public class TagVO {
+    @JsonIgnore // 忽略日记ID，不返回给前端
     private Long diaryId;
     private Long id;
     private String name;
